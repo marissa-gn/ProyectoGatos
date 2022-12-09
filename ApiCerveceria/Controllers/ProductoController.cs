6be2e5852fb131dfb82d840fafc6823b362b5e75
marissa-gn <gomnavmarissa@gmail.com>
2022-12-08 18:16:39 -0600

    cambios prueba mensaje

--- ApiCerveceria/Controllers/ProductoController.cs
+++ ApiCerveceria/Controllers/ProductoController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiCerveceria.Controllers
 {
+    //Este es un comentario de prueba 
     [Route("api/[controller]")]
     [ApiController]
     public class ProductoController : ControllerBase
